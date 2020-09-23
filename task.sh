b88f04e57e0950254321d859942f77e40827a607
SAYAN PAL <sayanp166@gmail.com>
2020-09-23 19:14:42 +0530

    task

---
+++ task.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+valid=1
+i=0
+while [ $valid -gt 0 ]
+do
+dice1=$(( RANDOM%6+1 ))
+dice2=$(( RANDOM%6+1 ))
+sum=$(( dice1+dice2 ))
+((i++))
+if [ $dice1 -eq 6 -a $dice2 -eq 6 ]
+then
+valid=0
+fi
+done
+echo "No. of Roll : $i"
+echo "1st Dice : $dice1 and 2 nd Dice : $dice2"
