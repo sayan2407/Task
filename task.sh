7fbfc4e004df81309d48fedad23f8fd5296ce4d4
sayan2407 <63643961+sayan2407@users.noreply.github.com>
2020-09-23 19:22:17 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -2,10 +2,12 @@
 
 valid=1
 i=0
+s=0
 while [ $valid -gt 0 ]
 do
 dice1=$(( RANDOM%6+1 ))
 dice2=$(( RANDOM%6+1 ))
+s=$(( s+dice1+dice2 ))
 sum=$(( dice1+dice2 ))
 ((i++))
 if [ $dice1 -eq 6 -a $dice2 -eq 6 ]
@@ -15,4 +17,4 @@ fi
 done
 echo "No. of Roll : $i"
 echo "1st Dice : $dice1 and 2 nd Dice : $dice2"
-echo "Sum of Roll is : $(( dice1+dice2 ))"
+echo "Sum of Roll is : $s"
